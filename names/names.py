bf3984976b7006b725cb6d24e8aa1794a59f3edb
Nick-Ballenger <nickster214@gmail.com>
2019-12-17 11:20:14 -0700

    mvp, still have a few errors in ring buffer

--- names/names.py
+++ names/names.py
@@ -1,5 +1,38 @@
 import time
 
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -11,10 +44,14 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+search_tree = BinarySearchTree(names_2[0])
+
+for name in names_1:
+    search_tree.insert(name)
+
+for name in names_2:
+    if search_tree.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
